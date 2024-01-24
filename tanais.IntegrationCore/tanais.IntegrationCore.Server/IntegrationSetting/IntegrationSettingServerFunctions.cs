728ccac99968bf8aa644e749c167a9519a2b298b
Pryamilov_VV <v.pryamilov@tanais.ru>
2024-01-24 16:06:25 +0300

    IntegrationSetting feat: добавлен серверный метод получения настроек интеграции по интегрированной системе.

--- tanais.IntegrationCore/tanais.IntegrationCore.Server/IntegrationSetting/IntegrationSettingServerFunctions.cs
+++ tanais.IntegrationCore/tanais.IntegrationCore.Server/IntegrationSetting/IntegrationSettingServerFunctions.cs
@@ -9,6 +9,27 @@ namespace tanais.IntegrationCore.Server
 {
   partial class IntegrationSettingFunctions
   {
-
+    
+    /// <summary>
+    /// Получить настройку интегрированной системы.
+    /// </summary>
+    /// <param name="integratedSystem">Интегрированная система.</param>
+    /// <returns>Настройка интегрированной системы.</returns>
+    [Public, Remote]
+    public tanais.IntegrationCore.IIntegrationSetting GetIntegrationSettingBySystem(tanais.IntegrationCore.IIntegratedSystem integratedSystem)
+    {
+      if (integratedSystem == null)
+        throw new Exception(tanais.IntegrationCore.IntegrationSettings.Resources.IntegratedSystemIsNoteFilled);
+      
+      var integrationSetting = tanais.IntegrationCore.IntegrationSettings
+        .GetAll(s => s.IntegratedSystem.Id == integratedSystem.Id)
+        .FirstOrDefault();
+      
+      if (integrationSetting == null)
+        throw new Exception(tanais.IntegrationCore.IntegrationSettings.Resources.IntegrationSettingNotFound);
+      
+      return integrationSetting;
+    }
+    
   }
 }

c58dc4de89e0b05abae9df771b8a861d4a862f6f
Pryamilov_VV <v.pryamilov@tanais.ru>
2024-01-25 12:37:19 +0300

    IntegrationSetting feat: обработка нажатия через SetOnButtonClick, возможность выполнения действий на форме.

--- tanais.IntegrationCore/tanais.IntegrationCore.ClientBase/IntegrationSetting/IntegrationSettingActions.cs
+++ tanais.IntegrationCore/tanais.IntegrationCore.ClientBase/IntegrationSetting/IntegrationSettingActions.cs
@@ -15,21 +15,26 @@ namespace tanais.IntegrationCore.Client
       var keyDialogField = inputDialog.AddPasswordString(tanais.IntegrationCore.IntegrationSettings.Resources.EnterKeyFieldTitle, true);
       var confirmKeyDialogField = inputDialog.AddPasswordString(tanais.IntegrationCore.IntegrationSettings.Resources.ConfirmKeyDialogFieldTitle, true);
       
-      if (inputDialog.Show() == DialogButtons.Ok)
-      {
-        if (String.Compare(keyDialogField.Value, confirmKeyDialogField.Value, StringComparison.Ordinal) == 0)
-        {
-          _obj.AccessKey = keyDialogField.Value;
-          _obj.VisibleAccessKey = new String('*', Constants.IntegrationSetting.NumberOfHidingCharacters);
-        }
-        else
-          e.AddError(tanais.IntegrationCore.IntegrationSettings.Resources.NotEqualsKeyMessage);
-      }
+      inputDialog.SetOnButtonClick((ed) =>
+                                   {
+                                     if (ed.Button == DialogButtons.Ok)
+                                     {
+                                       if (String.Compare(keyDialogField.Value, confirmKeyDialogField.Value, StringComparison.Ordinal) == 0)
+                                       {
+                                         _obj.AccessKey = keyDialogField.Value;
+                                         _obj.VisibleAccessKey = new String('*', Constants.IntegrationSetting.NumberOfHidingCharacters);
+                                       }
+                                       else
+                                         e.AddError(tanais.IntegrationCore.IntegrationSettings.Resources.NotEqualsKeyMessage);
+                                     }
+                                   });
+      
+      inputDialog.Show();
     }
 
     public virtual bool CanSpecifyKey(Sungero.Domain.Client.CanExecuteActionArgs e)
     {
-      return true;
+      return _obj.AuthMethod == tanais.IntegrationCore.IntegrationSetting.AuthMethod.Key;
     }
 
     public virtual void SetPassword(Sungero.Domain.Client.ExecuteActionArgs e)
@@ -38,21 +43,23 @@ namespace tanais.IntegrationCore.Client
       var passwordDialogField = inputDialog.AddPasswordString(tanais.IntegrationCore.IntegrationSettings.Resources.EnterPasswordFieldTitle, true);
       var confirmPasswordDialogField = inputDialog.AddPasswordString(tanais.IntegrationCore.IntegrationSettings.Resources.ConfirmPasswordFieldTitle, true);
       
-      if (inputDialog.Show() == DialogButtons.Ok)
-      {
-        if (String.Compare(passwordDialogField.Value, confirmPasswordDialogField.Value, StringComparison.Ordinal) == 0)
-        {
-          _obj.Password = passwordDialogField.Value;
-          _obj.VisiblePassword = new String('*', Constants.IntegrationSetting.NumberOfHidingCharacters);
-        }
-        else
-          e.AddError(tanais.IntegrationCore.IntegrationSettings.Resources.NotEqualsPasswordMessage);
-      }
+      inputDialog.SetOnButtonClick((ed) =>
+                                   {
+                                     if (String.Compare(passwordDialogField.Value, confirmPasswordDialogField.Value, StringComparison.Ordinal) == 0)
+                                     {
+                                       _obj.Password = passwordDialogField.Value;
+                                       _obj.VisiblePassword = new String('*', Constants.IntegrationSetting.NumberOfHidingCharacters);
+                                     }
+                                     else
+                                       e.AddError(tanais.IntegrationCore.IntegrationSettings.Resources.NotEqualsPasswordMessage);
+                                   });
+      
+      inputDialog.Show();
     }
 
     public virtual bool CanSetPassword(Sungero.Domain.Client.CanExecuteActionArgs e)
     {
-      return true;
+      return _obj.AuthMethod == tanais.IntegrationCore.IntegrationSetting.AuthMethod.Password;
     }
 
   }

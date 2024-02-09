d36b6fa16fe5dab14ae5910d05c7b25a2b3c373c
Pryamilov_VV <v.pryamilov@tanais.ru>
2024-02-09 13:45:10 +0300

    Initializing feat: созданы роли для работы с интеграцией, выданы права на справочники.

--- tanais.IntegrationCore/tanais.IntegrationCore.Server/ModuleInitializer.cs
+++ tanais.IntegrationCore/tanais.IntegrationCore.Server/ModuleInitializer.cs
@@ -9,5 +9,82 @@ namespace tanais.IntegrationCore.Server
 {
   public partial class ModuleInitializer
   {
+
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      CreateRoles();
+      GrantRightsOnEntities();
+    }
+    
+    #region Создание ролей
+
+    /// <summary>
+    /// Создать роли.
+    /// </summary>
+    public static void CreateRoles()
+    {
+      // Ответственные за интеграцию.
+      Sungero.Docflow.PublicInitializationFunctions.Module.CreateRole(
+        tanais.IntegrationCore.Resources.IntegrationResponsible,
+        tanais.IntegrationCore.Resources.IntegrationResponsible,
+        Constants.Module.RoleGuid.IntegrationResponsibleRole);
+      
+      // Пользователи с доступом к настройкам интеграции.
+      Sungero.Docflow.PublicInitializationFunctions.Module.CreateRole(
+        tanais.IntegrationCore.Resources.ReadIntegrationSettingRoleName,
+        tanais.IntegrationCore.Resources.ReadIntegrationSettingRoleName,
+        Constants.Module.RoleGuid.ReadIntegrationSettingRole);
+    }
+    
+    #endregion
+    
+    #region Выдача прав
+    
+    // <summary>
+    /// Выдать права на сущности.
+    /// </summary>
+    public static void GrantRightsOnEntities()
+    {
+      var readIntegrationSettingRole = GetRoleByGuid(Constants.Module.RoleGuid.ReadIntegrationSettingRole);
+      if (readIntegrationSettingRole != null)
+      {
+        InitializationLogger.Debug("Init: Grant rights on IntegrationSetting to readIntegrationSettingRole.");
+        GrantRightsOnEntityType(readIntegrationSettingRole, IntegrationSettings.AccessRights, DefaultAccessRightsTypes.Read);
+      }
+      
+      var integrationResponsibleRole = GetRoleByGuid(Constants.Module.RoleGuid.IntegrationResponsibleRole);
+      if (integrationResponsibleRole != null)
+      {
+        InitializationLogger.Debug("Init: Grant rights on IntegrationSetting to integrationResponsibleRole.");
+        GrantRightsOnEntityType(integrationResponsibleRole, IntegrationSettings.AccessRights, DefaultAccessRightsTypes.FullAccess);
+        
+        InitializationLogger.Debug("Init: Grant rights on IntegratedSystem to integrationResponsibleRole.");
+        GrantRightsOnEntityType(integrationResponsibleRole, IntegratedSystems.AccessRights, DefaultAccessRightsTypes.FullAccess);
+      }
+    }
+    
+    /// <summary>
+    /// Выдача прав доступа.
+    /// </summary>
+    [Public]
+    private static void GrantRightsOnEntityType(IRole role, Sungero.Domain.Shared.IEntityAccessRights accessRights, params Guid[] accessRightsTypes)
+    {
+      accessRights.Grant(role, accessRightsTypes);
+      accessRights.Save();
+    }
+
+    #endregion
+    
+    /// <summary>
+    /// Получить роль по Guid.
+    /// </summary>
+    /// <param name="roleGuid">Guid роли.</param>
+    /// <returns>Роль.</returns>
+    [Public]
+    public static IRole GetRoleByGuid(Guid roleGuid)
+    {
+      return Roles.GetAll(r => r.Sid == roleGuid).FirstOrDefault();
+    }
+    
   }
 }

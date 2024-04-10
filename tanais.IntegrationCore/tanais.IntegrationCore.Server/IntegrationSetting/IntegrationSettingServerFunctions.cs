41dfcbc9ec8d19854ddc96117f33af0b74b2a9cc
Fyamaev <f.yamaev@tanais.ru>
2024-04-10 18:02:57 +0300

    feat: решение с постфиксом системы будет перерабатываться.

--- tanais.IntegrationCore/tanais.IntegrationCore.Server/IntegrationSetting/IntegrationSettingServerFunctions.cs
+++ tanais.IntegrationCore/tanais.IntegrationCore.Server/IntegrationSetting/IntegrationSettingServerFunctions.cs
@@ -18,6 +18,30 @@ namespace Tanais.IntegrationCore.Server
     /// <returns>Настройка интегрированной системы.</returns>
     /// <remarks>Если настройка не найдена, будет создано исключение.</remarks>
     [Public, Remote]
+    public static Tanais.IntegrationCore.IIntegrationSetting GetIntegrationSettingBySystem(Tanais.IntegrationCore.IIntegratedSystem integratedSystem)
+    {
+      if (integratedSystem == null)
+        throw new Exception(Tanais.IntegrationCore.IntegrationSettings.Resources.IntegratedSystemIsNotSpecified);
+      
+      var integrationSetting = Tanais.IntegrationCore.IntegrationSettings
+        .GetAll()
+        .Where(s => s.IntegratedSystem.Id == integratedSystem.Id)
+        .FirstOrDefault();
+      
+      if (integrationSetting == null)
+        throw new Exception(Tanais.IntegrationCore.IntegrationSettings.Resources.IntegrationSettingNotFoundFormat(integratedSystem.Name));
+      
+      return integrationSetting;
+    }
+    
+    /// <summary>
+    /// Получить настройку интегрированной системы.
+    /// </summary>
+    /// <param name="integratedSystem">Интегрированная система.</param>
+    /// <param name="postfix">Постфикс.</param>
+    /// <returns>Настройка интегрированной системы.</returns>
+    /// <remarks>Если настройка не найдена, будет создано исключение.</remarks>
+    [Public, Remote]
     public static Tanais.IntegrationCore.IIntegrationSetting GetIntegrationSettingBySystem(Tanais.IntegrationCore.IIntegratedSystem integratedSystem, string postfix)
     {
       if (integratedSystem == null)
